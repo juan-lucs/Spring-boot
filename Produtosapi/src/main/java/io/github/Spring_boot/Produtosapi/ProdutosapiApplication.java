c1a31c59448274d8b352d7f1067373dc09cafa0a
juan_lucs <ls2699120@gmail.com>
2026-06-14 15:17:48 -0300

    Spring initializr

---
+++ Produtosapi/src/main/java/io/github/Spring_boot/Produtosapi/ProdutosapiApplication.java
@@ -0,0 +1,13 @@
+package io.github.Spring_boot.Produtosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdutosapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdutosapiApplication.class, args);
+	}
+
+}
