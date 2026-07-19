b64582245a5913d3bb4cf8379ecf593c50c7ce86
juan_lucs <ls2699120@gmail.com>
2026-07-19 00:00:39 -0300

    criado o a conexão usando o HIKARI

--- api-Livraria/src/main/java/io/github/springboot/apilivraria/config/DatabaseConfiguration.java
+++ api-Livraria/src/main/java/io/github/springboot/apilivraria/config/DatabaseConfiguration.java
@@ -1,5 +1,7 @@
 package io.github.springboot.apilivraria.config;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import org.jspecify.annotations.NonNull;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -19,17 +21,41 @@ public class DatabaseConfiguration {
     private String password;
     @Value("${spring.datasource.driver-class-name}")
     private String driver;
-// TUDO ISSO A BAIXO É UMA IMPLEMENTAÇÃO SIMPLES DE UMA CONEXÃO, NÃO É RECOMENDADO PARA PRODUÇÃO REAL
+// TUDO ISSO A BAIXO É UMA IMPLEMENTAÇÃO SIMPLES DE UMA CONEXÃO, NÃO É RECOMENDADO PARA PRODUÇÃO REAL, APENAS COMENTANDO O BEAN, O SPRING JA IGNORA ISSO
 //    @Bean
 //    @NonNull
-//    private DataSource datasource(){
-//        DriverManagerDataSource ds = new DriverManagerDataSource(); //DriverManagerDataSource IPLEMENTA de DataSource, por isso eu posso retornar ele. polimorfismo no sangue!!!
-//        ds.setUrl(url);
-//        ds.setUsername(username);
-//        ds.setPassword(password);
-//        ds.setDriverClassName(driver);
-//        return ds;
-//    };
-
-
+    private DataSource datasource(){
+        DriverManagerDataSource ds = new DriverManagerDataSource(); //DriverManagerDataSource IPLEMENTA de DataSource, por isso eu posso retornar ele. polimorfismo no sangue!!!
+        ds.setUrl(url);
+        ds.setUsername(username);
+        ds.setPassword(password);
+        ds.setDriverClassName(driver);
+        return ds;
+    };
+    @Bean
+    // O HIKARI É O PADRÃO DA CRIAÇÃO NO SPRINGBOOT, SE EU DEIXAR O BEAN COMENTADO ELE JÁ VAI FAZER TUDO ISSO
+    public DataSource hikariDataSource() {
+        // Objeto que armazena todas as configurações do pool.
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(url);
+        config.setUsername(username);
+        config.setPassword(password);
+        config.setDriverClassName(driver);
+        // Número máximo de conexões que podem existir ao mesmo tempo.
+        config.setMaximumPoolSize(10);
+        //
+        config.setMinimumIdle(1);
+        // Nome do pool (aparece em logs e ferramentas de monitoramento).
+        config.setPoolName("livraria-db-pool");
+        // Tempo máximo que uma conexão pode ficar viva (10 minutos). Depois desse tempo ela é descartada e substituída.
+        config.setMaxLifetime(600000);
+        // Tempo máximo de espera para conseguir uma conexão do pool.Se passar desse tempo, uma exceção será lançada.
+        config.setConnectionTimeout(100000);
+        // Consulta usada para verificar se a conexão ainda está funcionando. Muitos drivers nem precisam disso atualmente.
+        config.setConnectionTestQuery("SELECT 1");
+        return new HikariDataSource(config);
+    }
+    // Diferente do DriverManagerDataSource, o HikariCP não cria uma conexão
+// toda vez que o banco é acessado. Ele mantém um conjunto de conexões
+// prontas e as empresta para a aplicação quando necessário.
 }
